826ffe84389b33ded055fc7b8996d53b9544baee
LorenzoRiosWeb <100882991+LorenzoRiosWeb@users.noreply.github.com>
2024-03-19 20:10:40 -0600

    added function for connection to the sql, also added comments for creating certain functions

--- server.js
+++ server.js
@@ -8,14 +8,47 @@ const dbConfig = {
     password: process.env.DB_PASSWORD
 };
 
-async function connectToDatabase() { // Mark the function as async
+async function connectToDatabase() {
     try {
         const connection = await mysql.createConnection(dbConfig);
         return connection;
     } catch (error) {
         console.log('error connecting to the database:', error);
-        // You might want to throw the error to handle it elsewhere
-        throw error;
+        process.env(1);
     }
 }
 
+// Create a function to view all departments
+async function viewAllDepartments(connection) {}
+
+// Create a function to view all roles
+async function viewAllRoles(connection) {}
+
+
+// Create function to view all employees
+
+async function viewAllEmployees(connection) {}
+
+// Function to add department
+
+async function addDepartment(connection){
+
+}
+
+// Create function to add role
+
+async function addRole(connection){
+
+}
+
+// add employee function
+
+async function addEmployee(connection) {}
+
+// function to update the employee role
+
+async function updateEmployeeRole(connection) {}
+
+// main function
+
+async function main() {}
